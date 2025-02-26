e9876958f09512c7ebd090a2eec20d183f337dbb
MrVinh5342 <nguyennhat02102006@gmail.com>
2025-02-26 09:35:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạnn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Wed này là do thiên tài nhà làm",
+    introTitle: 'Bố vinh đêyyy',
+    introDesc: `Trái đất hình củ khoai tây
+    củ khoai tây luộc chín`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'phải chăng em đã thích ăn khoai tây từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc em ăn sống củ khoai tâyyy ',
+    btnYes: 'Thích ăn khoai tây lắmmm <33',
+    btnNo: 'khôn ăn cho 10 tỉ :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn ăn củ khoai tây thay vì củ cà dốt <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'ăn mà không ăn thì cho 10 tỉ<33333333',
+    mess: 'Mình biết mà 🥰. mình rất thích ăn củ cà chua😘😘',
+    messDesc: 'Tối nay 10h mình đón bạn vào minecraft ăn khoai tây nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
