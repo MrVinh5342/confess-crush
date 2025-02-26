7c0a8971d549eff4ff10f067f52b280113a6ecb6
MrVinh5342 <nguyennhat02102006@gmail.com>
2025-02-26 09:16:18 +0700

    Update and rename config.js to Mrvinh.h
    yeppp

---
+++ Mrvinh.h
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Bố vinh đêy',
+    introDesc: `nhảm nhí vcl
+    tự dưng đi làm mấy trò vớ vẩn`,
+    btnIntro: '^^HiHi^^',
+    title: 'hình như em đã muốn đòi nợ a từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã muốn đấm a từ cái nhếch mắt thứ 2 ',
+    btnYes: 'Yêu vinh vcl <33',
+    btnNo: 'từ chối làm chó :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại ấn vào chữ đó <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì sang campuchia<33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay chơi ép ép nha.',
+    btnAccept: 'anh Vinh muôn năm <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
