56ef4ab0771b2f72476903783717dae042401bf3
MrVinh5342 <nguyennhat02102006@gmail.com>
2025-02-26 08:59:05 +0700

    MrVinhh

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    introTitle: 'Chào bố Vinh điii',
+    introDesc: `Trái đất hình tròn đúng không em
+    Mặt trăng có chéo không`,
+    btnIntro: '^^Cái củ cặc^^',
+    title: 'Phải chăng em đã muốn đấm ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã muốn nện ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'cho 100 tỉ cũng đc <33',
+    btnNo: 'Cho anh 10 tỉ nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại muốn cho mình 10 tỉ <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Tối nay 10h, mình chơi game nha.',
+    btnAccept: 'tao đồng ý <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
